49677c0b38386e6207fc08d4c59c4539eeee3f42
Antonis <lolplayer1231@outlook.com>
2026-02-21 12:27:44 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace Payroll_Sedoro
+    
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string empid = "kirby";
+            int hourlyRate = 600, hoursWorked = 80;
+            int gross = hoursWorked * hourlyRate;
+            int overtimeHours = 5;
+            double overtimeIncrease = 1.25; 
+            double overtimeAmount = hourlyRate * overtimeIncrease;
+            double overtimeTotal = overtimeHours * overtimeAmount;
+            double leave = 1;
+            double leaveDeduction = leave * hourlyRate;
+            double totalGross = (gross + overtimeTotal) - leaveDeduction;
+            double pagibigBiweekly = 200 / 2;
+            double netPay = totalGross - pagibigBiweekly;
+
+
+            Console.WriteLine("Employee Management System");
+            Console.WriteLine("PAYROLL OF BIWEEKLY");
+
+            Console.Write("Enter Employee ID: ");
+            string userEmpid = Console.ReadLine();
+
+            if (userEmpid == empid)
+            {
+                Console.WriteLine($"Hourly Rate: {hourlyRate}");
+                Console.WriteLine($"Hours Worked: {hoursWorked}");
+                Console.WriteLine($"Gross Basic Pay: {gross}");
+                Console.WriteLine($"Overtime ({overtimeHours} hrs.): {overtimeTotal}");
+                Console.WriteLine($"Leave ({leave} day/s): {leaveDeduction}");
+                Console.WriteLine($"Total Gross Pay: {totalGross}");
+                Console.WriteLine("");
+
+                Console.WriteLine($"PAG-IBIG Tax: {pagibigBiweekly}");
+                Console.WriteLine("");
+                Console.WriteLine($"NETPAY: {netPay}");
+            }
+
+        }
+    }
+}
