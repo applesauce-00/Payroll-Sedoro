a3b31bb7d98cb816a15982731a5bef210e3a8989
aina0331 <buthaina3106@gmail.com>
2026-02-28 12:23:08 +0800

    add additional name

--- Program.cs
+++ Program.cs
@@ -1,43 +1,46 @@
 ﻿using System;
-namespace payroll
+namespace Payroll_Sedoro
 
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string empid = "m";
+            string empid = "kirby";
             int hourlyRate = 600, hoursWorked = 80;
-
-
             int overtimeHours = 3;
             double overtimeIncrease = 1.25;
-
             double leave = 1;
             double pagibigBiweekly = 200 / 2;
 
             Console.Write("Enter Employee ID: ");
             string userEmpid = Console.ReadLine();
 
-            if (isValidEmployee(userEmpid, empid))
             {
-                double gross = grossComputation(hourlyRate, hoursWorked);
-                double overtimeTotal = overtimeComputation(hourlyRate, overtimeHours, overtimeIncrease);
-                double leaveDeduction = leaveDeductionComputation(hourlyRate, leave);
-                double totalGross = totalGrossComputation(gross, overtimeTotal, leaveDeduction);
-                double netPay = netPayComputation(totalGross, pagibigBiweekly);
-                displayPayroll(userEmpid, hourlyRate, hoursWorked, gross, overtimeHours, overtimeTotal, leaveDeduction, totalGross, pagibigBiweekly, netPay);
-            }
-            else
-            {
-                Console.WriteLine("Incorrect Employee ID");
+                if (isValidEmployee(userEmpid, empid))
+                {
+                    double gross = grossComputation(hourlyRate, hoursWorked);
+                    double overtimeTotal = overtimeComputation(hourlyRate, overtimeHours, overtimeIncrease);
+                    double leaveDeduction = leaveDeductionComputation(hourlyRate, leave);
+                    double totalGross = totalGrossComputation(gross, overtimeTotal, leaveDeduction);
+                    double netPay = netPayComputation(totalGross, pagibigBiweekly);
+
+                    displayPayroll(userEmpid, hourlyRate, hoursWorked, gross, overtimeHours, overtimeTotal, leaveDeduction, totalGross, pagibigBiweekly, netPay);
+                    Console.WriteLine("");
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect Employee ID");
+                }
+
             }
 
         }
 
-        static bool isValidEmployee(string inputId, string validId)
+
+        static bool isValidEmployee(string inputId, string correctId)
         {
-            return inputId == validId;
+            return inputId == correctId;
         }
 
         static double overtimeComputation(int rate, double hours, double increase)
@@ -63,9 +66,18 @@ namespace payroll
         }
         static void displayPayroll(string empid, int hourlyRate, int hoursWorked, double gross, int overtimeHours, double overtimeTotal, double leaveDeduction, double totalGross, double pagibigBiweekly, double netPay)
         {
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("------------------------------------");
             Console.WriteLine("Employee Management System");
-            Console.WriteLine("PAYROLL OF BIWEEKLY");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("PAYROLL");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("Method of Salary: Bi-weekly");
             Console.Write($"Employee ID: {empid}\n");
+            Console.Write($"Employee Name: Kirby T. Sedoro");
+            Console.WriteLine("");
             Console.WriteLine($"Hourly Rate: {hourlyRate}");
             Console.WriteLine($"Hours Worked: {hoursWorked}");
             Console.WriteLine($"Gross Basic Pay: {gross}");
@@ -74,10 +86,10 @@ namespace payroll
             Console.WriteLine($"Total Gross Pay: {totalGross}");
             Console.WriteLine("");
             Console.WriteLine($"PAG-IBIG: {pagibigBiweekly}");
-            //Console.WriteLine($"SSS: {}");
-            //Console.WriteLine($"PhilHealth: {pagibigBiweekly}");
             Console.WriteLine("");
             Console.WriteLine($"NETPAY: {netPay}");
         }
     }
+
+
 }
