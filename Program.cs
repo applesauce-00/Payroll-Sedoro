f4a2778e0ac30829b337d7e45361268843ec1ac4
aina0331 <buthaina3106@gmail.com>
2026-02-28 11:32:48 +0800

    added function for cleaner looks

--- Program.cs
+++ Program.cs
@@ -1,55 +1,83 @@
 ﻿using System;
-namespace Payroll_Sedoro
-    
+namespace payroll
+
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string empid = "kirby";
+            string empid = "m";
             int hourlyRate = 600, hoursWorked = 80;
-            int gross = hoursWorked * hourlyRate;
-            int overtimeHours = 5;
-            double overtimeIncrease = 1.25; 
-            double overtimeAmount = hourlyRate * overtimeIncrease;
-            double overtimeTotal = overtimeHours * overtimeAmount;
+
+
+            int overtimeHours = 3;
+            double overtimeIncrease = 1.25;
+
             double leave = 1;
-            double leaveDeduction = leave * hourlyRate;
-            double totalGross = totalGrossComputation(gross, overtimeTotal, leaveDeduction);
             double pagibigBiweekly = 200 / 2;
-            double netPay = totalGross - pagibigBiweekly;
-
-
-            Console.WriteLine("Employee Management System");
-            Console.WriteLine("PAYROLL OF BIWEEKLY");
 
             Console.Write("Enter Employee ID: ");
             string userEmpid = Console.ReadLine();
 
-            if (userEmpid == empid)
+            if (isValidEmployee(userEmpid, empid))
             {
-                Console.WriteLine($"Hourly Rate: {hourlyRate}");
-                Console.WriteLine($"Hours Worked: {hoursWorked}");
-                Console.WriteLine($"Gross Basic Pay: {gross}");
-                Console.WriteLine($"Overtime ({overtimeHours} hrs.): {overtimeTotal}");
-                Console.WriteLine($"Leave ({leave} day/s): {leaveDeduction}");
-                Console.WriteLine($"Total Gross Pay: {totalGross}");
-                Console.WriteLine("");
-
-                Console.WriteLine($"PAG-IBIG Tax: {pagibigBiweekly}");
-                Console.WriteLine("");
-                Console.WriteLine($"NETPAY: {netPay}");
-            }  else
+                double gross = grossComputation(hourlyRate, hoursWorked);
+                double overtimeTotal = overtimeComputation(hourlyRate, overtimeHours, overtimeIncrease);
+                double leaveDeduction = leaveDeductionComputation(hourlyRate, leave);
+                double totalGross = totalGrossComputation(gross, overtimeTotal, leaveDeduction);
+                double netPay = netPayComputation(totalGross, pagibigBiweekly);
+                displayPayroll(userEmpid, hourlyRate, hoursWorked, gross, overtimeHours, overtimeTotal, leaveDeduction, totalGross, pagibigBiweekly, netPay);
+            }
+            else
             {
-                Console.WriteLine("Invalid Employee ID");
+                Console.WriteLine("Incorrect Employee ID");
             }
 
         }
 
+        static bool isValidEmployee(string inputId, string validId)
+        {
+            return inputId == validId;
+        }
+
+        static double overtimeComputation(int rate, double hours, double increase)
+        {
+            return rate * (hours * increase);
+        }
+        static double leaveDeductionComputation(int rate, double leave)
+        {
+            return rate * leave;
+        }
+        static double grossComputation(int rate, int hours)
+        {
+            return rate * hours;
+        }
         static double totalGrossComputation(double gross, double overtimeTotal, double leaveDeduction)
         {
             double totalGross = (gross + overtimeTotal) - leaveDeduction;
-        
+            return totalGross;
+        }
+        static double netPayComputation(double totalGross, double pagibig)
+        {
+            return totalGross - pagibig;
+        }
+        static void displayPayroll(string empid, int hourlyRate, int hoursWorked, double gross, int overtimeHours, double overtimeTotal, double leaveDeduction, double totalGross, double pagibigBiweekly, double netPay)
+        {
+            Console.WriteLine("Employee Management System");
+            Console.WriteLine("PAYROLL OF BIWEEKLY");
+            Console.Write($"Employee ID: {empid}\n");
+            Console.WriteLine($"Hourly Rate: {hourlyRate}");
+            Console.WriteLine($"Hours Worked: {hoursWorked}");
+            Console.WriteLine($"Gross Basic Pay: {gross}");
+            Console.WriteLine($"Overtime ({overtimeHours} hr/s,): {overtimeTotal}");
+            Console.WriteLine($"Leave Deduction: {leaveDeduction}");
+            Console.WriteLine($"Total Gross Pay: {totalGross}");
+            Console.WriteLine("");
+            Console.WriteLine($"PAG-IBIG: {pagibigBiweekly}");
+            //Console.WriteLine($"SSS: {}");
+            //Console.WriteLine($"PhilHealth: {pagibigBiweekly}");
+            Console.WriteLine("");
+            Console.WriteLine($"NETPAY: {netPay}");
         }
     }
 }
