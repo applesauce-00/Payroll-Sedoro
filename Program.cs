ca29dc575ff34798e8ba91f957b6e62a085e4557
Kirby Sedoro <kirbysedoro@gmail.com>
2026-03-13 13:56:19 +0800

    .

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
+<<<<<<< HEAD
 ﻿using PayrollData;
 using PayrollService;
 using PayrollDataLayer;
 using PayrollDataService;
+=======
+﻿using System;
+namespace payroll
+>>>>>>> parent of a3b31bb (add additional name)
 
 namespace Payroll_Sedoro
 {
@@ -9,13 +14,26 @@ namespace Payroll_Sedoro
     {
         static void Main(string[] args)
         {
+<<<<<<< HEAD
             EmployeeInfos info = new EmployeeInfos();
             PayrollServices payroll = new PayrollServices();
             Employee emp = info.GetEmployee();
+=======
+            string empid = "m";
+            int hourlyRate = 600, hoursWorked = 80;
+
+
+            int overtimeHours = 3;
+            double overtimeIncrease = 1.25;
+
+            double leave = 1;
+            double pagibigBiweekly = 200 / 2;
+>>>>>>> parent of a3b31bb (add additional name)
 
             short attempt = 0;
             bool valid = false;
 
+<<<<<<< HEAD
             while (attempt < 3 && !valid)
             {
                 Console.Write("Enter Employee ID: ");
@@ -45,9 +63,24 @@ namespace Payroll_Sedoro
                         return;
                     }
                 }
+=======
+            if (isValidEmployee(userEmpid, empid))
+            {
+                double gross = grossComputation(hourlyRate, hoursWorked);
+                double overtimeTotal = overtimeComputation(hourlyRate, overtimeHours, overtimeIncrease);
+                double leaveDeduction = leaveDeductionComputation(hourlyRate, leave);
+                double totalGross = totalGrossComputation(gross, overtimeTotal, leaveDeduction);
+                double netPay = netPayComputation(totalGross, pagibigBiweekly);
+                displayPayroll(userEmpid, hourlyRate, hoursWorked, gross, overtimeHours, overtimeTotal, leaveDeduction, totalGross, pagibigBiweekly, netPay);
+            }
+            else
+            {
+                Console.WriteLine("Incorrect Employee ID");
+>>>>>>> parent of a3b31bb (add additional name)
             }
         }
 
+<<<<<<< HEAD
         static void DisplayPayroll(Employee emp, double gross, double overtime,
             double leaveDeduction, double totalGross, double netPay, PayrollServices payroll)
         {
@@ -64,10 +97,46 @@ namespace Payroll_Sedoro
 
             Console.WriteLine($"\nHourly Rate: {emp.HourlyRate}");
             Console.WriteLine($"Hours Worked: {emp.HoursWorked}");
+=======
+        static bool isValidEmployee(string inputId, string validId)
+        {
+            return inputId == validId;
+        }
+
+        static double overtimeComputation(int rate, double hours, double increase)
+        {
+            return rate * (hours * increase);
+        }
+        static double leaveDeductionComputation(int rate, double leave)
+        {
+            return rate * leave;
+        }
+        static double grossComputation(int rate, int hours)
+        {
+            return rate * hours;
+        }
+        static double totalGrossComputation(double gross, double overtimeTotal, double leaveDeduction)
+        {
+            double totalGross = (gross + overtimeTotal) - leaveDeduction;
+            return totalGross;
+        }
+        static double netPayComputation(double totalGross, double pagibig)
+        {
+            return totalGross - pagibig;
+        }
+        static void displayPayroll(string empid, int hourlyRate, int hoursWorked, double gross, int overtimeHours, double overtimeTotal, double leaveDeduction, double totalGross, double pagibigBiweekly, double netPay)
+        {
+            Console.WriteLine("Employee Management System");
+            Console.WriteLine("PAYROLL OF BIWEEKLY");
+            Console.Write($"Employee ID: {empid}\n");
+            Console.WriteLine($"Hourly Rate: {hourlyRate}");
+            Console.WriteLine($"Hours Worked: {hoursWorked}");
+>>>>>>> parent of a3b31bb (add additional name)
             Console.WriteLine($"Gross Basic Pay: {gross}");
             Console.WriteLine($"Overtime ({emp.OvertimeHours} hr/s): {overtime}");
             Console.WriteLine($"Leave Deduction ({emp.LeaveDays} day/s): {leaveDeduction}");
             Console.WriteLine($"Total Gross Pay: {totalGross}");
+<<<<<<< HEAD
 
             Console.WriteLine("\nTAXES");
             Console.WriteLine($"PAG-IBIG: {tax.Pagibig}");
@@ -79,3 +148,14 @@ namespace Payroll_Sedoro
         }
     }
 }
+=======
+            Console.WriteLine("");
+            Console.WriteLine($"PAG-IBIG: {pagibigBiweekly}");
+            //Console.WriteLine($"SSS: {}");
+            //Console.WriteLine($"PhilHealth: {pagibigBiweekly}");
+            Console.WriteLine("");
+            Console.WriteLine($"NETPAY: {netPay}");
+        }
+    }
+}
+>>>>>>> parent of a3b31bb (add additional name)
